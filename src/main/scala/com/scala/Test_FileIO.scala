c927f71a34336dc9e713a68952866b6248aa3d29
LittleSun <xuys01@foxmail.com>
2021-06-17 09:51:47 +0800

    优化代码

--- src/main/scala/com/scala/Test_FileIO.scala
+++ src/main/scala/com/scala/Test_FileIO.scala
@@ -6,18 +6,20 @@ import java.io.{File, PrintWriter}
 import scala.io.Source
 
 /**
- * @Author xys
- * @Version 1.0.0
- * @Description 文件输入输出
- * @createTime 2021年04月27日 14:27:00
+ * 文件输入输出
+ *
+ * @author xys
+ * @version 1.0.0
  */
 object Test_FileIO {
   def main(args: Array[String]): Unit = {
 
     //从文件读取数据
-    Source.fromFile("src/main/resources/test.txt").foreach(print)
+    val source = Source.fromFile("src/main/resources/test.txt")
+    source.foreach(println)
+    source.close()
     //写入数据到文件
-    val writer= new PrintWriter(new File("src/main/resources/test.txt"))
+    val writer = new PrintWriter(new File("src/main/resources/test.txt"))
     writer.write("java io hello")
     writer.close()
   }
