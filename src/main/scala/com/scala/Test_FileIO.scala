8de3b5b78f69bc0a6231ec2241adb7fa09cea8c6
LittleSun <xuys01@foxmail.com>
2021-06-18 16:38:51 +0800

    更新

--- src/main/scala/com/scala/Test_FileIO.scala
+++ src/main/scala/com/scala/Test_FileIO.scala
@@ -15,8 +15,15 @@ object Test_FileIO {
   def main(args: Array[String]): Unit = {
     //从文件读取数据
     val source = Source.fromFile("src/main/resources/test.txt")
-    source.foreach(println)
+    val str = source.toList.mkString("")
+    //关闭源
     source.close()
+    //把源数据转为list集合
+    val list = List(str)
+    //往集合内添加新数据
+    val newList: List[String] = list :+ "1"
+    //打印新集合
+    newList.foreach(println)
     //写入数据到文件
     val writer = new PrintWriter(new File("src/main/resources/test.txt"))
     writer.write("java io hello")
